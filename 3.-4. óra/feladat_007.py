05b8c2848de456670467445c1ba86512f7ace441
Bercel05 <113088498+Bercel05@users.noreply.github.com>
2023-10-06 13:02:10 +0200

    Add files via upload

---
+++ 3.-4. óra/feladat_007.py
@@ -0,0 +1,10 @@
+gondolt_szám = 4 
+tipp = input('Gondoltam egy számra. Tippeld meg!')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print(f'Ügyes')
+    print('Pápá')
+else:
+        print('Hosszan gondolkodtál rajta?:)')
+        print('Nem érte meg.;')
+        print('Pápá.')
